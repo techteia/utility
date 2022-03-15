f559feef70e05ec506b984f26e5295bd19b18684
MrSwirlyEyes <profswirlyeyes@gmail.com>
2022-03-14 23:48:40 -0700

    Fix module exports.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.createDatetimeFromString = (datetimeString) => {
+const createDatetimeFromString = (datetimeString) => {
   const millisecond = 1;
   const second = 1000 * millisecond;
   let datetime;
@@ -12,21 +12,21 @@ module.exports.createDatetimeFromString = (datetimeString) => {
 };
 
 // Decodes base64 strings to ASCII
-module.exports.decodeBase64 = (data, encoding = 'base64', radix = 'utf-8') => {
+const decodeBase64 = (data, encoding = 'base64', radix = 'utf-8') => {
   // const buff = Buffer.from(data, encoding);
   // return buff.toString(radix);
   return Buffer.from(data, encoding).toString(radix);
 };
 
 // Encodes strings to base64
-module.exports.encodeBase64 = (data, encoding = 'base64') => {
+const encodeBase64 = (data, encoding = 'base64') => {
   // const buff = Buffer.from(data);
   // return buff.toString(encoding);
   return Buffer.from(data).toString(encoding);
 };
 
 // Current datetime in following format (string) YYYY-MM-DD HH:MM:SS
-module.exports.getCondensedDatetimeString = (_datetime = new Date()) => {
+const getCondensedDatetimeString = (_datetime = new Date()) => {
   const _year = _datetime.getFullYear();
   const _month = zeroPadInteger(_datetime.getMonth() + 1, 2);
   const _date = zeroPadInteger(_datetime.getDate(), 2);
@@ -38,7 +38,7 @@ module.exports.getCondensedDatetimeString = (_datetime = new Date()) => {
 };
 
 // Current datetime in following format (string) YYYY-MM-DD HH:MM:SS
-module.exports.getFormattedDatetimeString = (datetime = new Date()) => {
+const getFormattedDatetimeString = (datetime = new Date()) => {
   const year = datetime.getFullYear();
   const month = zeroPadInteger(datetime.getMonth(), 2);
   const date = zeroPadInteger(datetime.getDate(), 2);
@@ -50,15 +50,26 @@ module.exports.getFormattedDatetimeString = (datetime = new Date()) => {
   return (`${year}-${month}-${date} ${hours}:${minutes}:${seconds}`);
 };
 
-module.exports.log = (...args) => {
+const log = (...args) => {
   console.info(`[${this.getFormattedDatetimeString()}] ${args.reduce((accumulator, value) => `${accumulator} ${value}`)}`);
 };
 
 // Performs a url encoding to a javascript object
-module.exports.urlEncode = (o) => Object.keys(o).map((k) => `${encodeURIComponent(k)}=${encodeURIComponent(o[k])}`).join('&');
+const urlEncode = (o) => Object.keys(o).map((k) => `${encodeURIComponent(k)}=${encodeURIComponent(o[k])}`).join('&');
 
 // Appends 0's to a number and returns as a string
-module.exports.zeroPadInteger = (integer, fieldWidth) => {
+const zeroPadInteger = (integer, fieldWidth) => {
   integer = integer + '';
   return integer.length >= fieldWidth ? integer : new Array(fieldWidth - integer.length + 1).join('0') + integer;
+};
+
+module.exports = {
+  createDatetimeFromString,
+  decodeBase64,
+  encodeBase64,
+  log,
+  getCondensedDatetimeString,
+  getFormattedDatetimeString,
+  urlEncode,
+  zeroPadInteger,
 };
